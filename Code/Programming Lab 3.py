1f0e541ff05e457bb2daf4f6a425370faf29ede2
savxsouth <savannahjsouthward@gmail.com>
2022-04-12 00:09:13 -0500

    Updated Documentation for Lab 3

--- Code/Programming Lab 3.py
+++ Code/Programming Lab 3.py
@@ -38,11 +38,10 @@ Created on Mon Apr 11 22:36:36 2022
 #  * Maximum, Minimum, and Average Summary Reports. 
 #  
 # To Do: 
-#  * FIX THIS
-#  * Ask if this is even what I am supposed to be doing
+#  * N?A
 #  
 # Bugs: 
-#  * This whole thing is a mess
+#  * None, that I know of...
 # 
 # Notes: 
 #   * All Python libraries are the latest versions as of release date. 
@@ -64,31 +63,29 @@ from pathlib import Path
 from datetime import datetime, timedelta
 
 # Choose the location of the data file
-## Choose the location of the data file
 settings_file = Path('/Users/savannahsouthward/opt/anaconda3/envs/METR-2613/Code/settings.yaml') 
 with open(settings_file, "r") as f:
     settings = yaml.safe_load(f)
 print(settings)
 
+# Use yaml settings file to assign time sensitive variables 
 start_date = datetime.strptime(settings['start_date'], "%Y-%m-%d %H:%M")
 end_date = datetime.strptime(settings['end_date'], "%Y-%m-%d %H:%M")
 current_date = start_date
 
-## Raw data file without missing data filled 
+# Data frame of the raw data file without missing data filled 
 raw_datafile = pd.read_csv(settings["data_file"], skiprows = [0, 2, 3])
 raw_datafile["TIMESTAMP"] = pd.to_datetime(raw_datafile["TIMESTAMP"])
 
-# Summary Report with QA-ed Data
-# Create the formatted Summary Report File within desired directory
-os.chdir(settings["output_file_path"])
-
+# (Insert Passive Aggressive Comment about Datetime Here...)
 settings['start_date'] = datetime.strptime(settings['start_date'], "%Y-%m-%d %H:%M")
 settings['end_date'] = datetime.strptime(settings['end_date'], "%Y-%m-%d %H:%M")
 
+# Blank data frame to be used to appease the index function
 empty = pd.DataFrame(index=pd.date_range(settings['start_date'], settings['end_date'], freq='5min'),
                         columns=['RECORD','TAIR','RELH','SRAD','WSPD','WMAX','WDIR','RAIN','BATV']).rename_axis('TIMESTAMP')
 
-# Read in raw data file
+# Read in raw data file to be corrected 
 rawdata = pd.read_csv(settings['data_file'], skiprows=[0,2,3], index_col='TIMESTAMP')
 rawdata.index = pd.to_datetime(rawdata.index)
 
@@ -96,27 +93,37 @@ rawdata.index = pd.to_datetime(rawdata.index)
 data = rawdata.combine_first(empty)
 data.reset_index(inplace = True)
 
-## Parse data for the particular day 
+# Parse data for the particular day 
 data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
 data = data.drop('RECORD', axis=1)
-
 start_date = settings['start_date']
 end_date = settings['end_date']
 
-data["TAIR"] = data["TAIR"].apply(lambda row: -998 if ((row < settings["variable"]["TAIR"]["QA"]["low_limit"]) or (row > settings["variable"]["TAIR"]["QA"]["high_limit"])) else row)
-data["RELH"] = data["RELH"].apply(lambda row: -998 if ((row < settings["variable"]["RELH"]["QA"]["low_limit"]) or (row > settings["variable"]["RELH"]["QA"]["high_limit"])) else row)
-data["SRAD"] = data["SRAD"].apply(lambda row: -998 if ((row < settings["variable"]["SRAD"]["QA"]["low_limit"]) or (row > settings["variable"]["SRAD"]["QA"]["high_limit"])) else row)
-data["WSPD"] = data["WSPD"].apply(lambda row: -998 if ((row < settings["variable"]["WSPD"]["QA"]["low_limit"]) or (row > settings["variable"]["WSPD"]["QA"]["high_limit"])) else row)
-data["WMAX"] = data["WMAX"].apply(lambda row: -998 if ((row < settings["variable"]["WMAX"]["QA"]["low_limit"]) or (row > settings["variable"]["WMAX"]["QA"]["high_limit"])) else row)
-                                                                                                                                                           
+# Assign QA flags to the 'data' data frame
+data["TAIR"] = data["TAIR"].apply(lambda row: -998 if ((row < settings["variable"]["TAIR"]["QA"]["low_limit"]) 
+                                                       or (row > settings["variable"]["TAIR"]["QA"]["high_limit"])) else row)
+data["RELH"] = data["RELH"].apply(lambda row: -998 if ((row < settings["variable"]["RELH"]["QA"]["low_limit"]) 
+                                                       or (row > settings["variable"]["RELH"]["QA"]["high_limit"])) else row)
+data["SRAD"] = data["SRAD"].apply(lambda row: -998 if ((row < settings["variable"]["SRAD"]["QA"]["low_limit"]) 
+                                                       or (row > settings["variable"]["SRAD"]["QA"]["high_limit"])) else row)
+data["WSPD"] = data["WSPD"].apply(lambda row: -998 if ((row < settings["variable"]["WSPD"]["QA"]["low_limit"]) 
+                                                       or (row > settings["variable"]["WSPD"]["QA"]["high_limit"])) else row)
+data["WMAX"] = data["WMAX"].apply(lambda row: -998 if ((row < settings["variable"]["WMAX"]["QA"]["low_limit"]) 
+                                                       or (row > settings["variable"]["WMAX"]["QA"]["high_limit"])) else row)
+
+# Copy of 'data' data frame to be used for our QA statistics                                                                                                                                                            
 qa_stats = data.copy()                                                                                                                                                      
 qa_stats = qa_stats.replace(-998, np.nan)
 
+# Fill the 'data' data frame with -9999 because we hate nans
 data.fillna('-9999', inplace=True)
 
+# Designate variables to be called when naming our parsed data files
 sumdate_start =  pd.to_datetime(settings['start_date']).strftime('%Y%m%d')
 sumdate_end =  pd.to_datetime(settings['end_date']).strftime('%Y%m%d')
 
+# Create the formatted Summary Report with QA-ed Data File within desired directory
+os.chdir(settings["output_file_path"])
 qa_summary = "NWC0_REPORT_" + sumdate_start + "_" + sumdate_end   
 file = open(qa_summary+'.txt', 'w')
 file.write("Statistics Report (QA) \n" "Input file: " + settings["data_filename"] 
@@ -125,27 +132,26 @@ file.write("Statistics Report (QA) \n" "Input file: " + settings["data_filename"
 # While Loop to cycle through the data for each day
 while current_date <= end_date:
     print(current_date)
+
+# Set up a variable from 00z to 23:55z using our 'data' data frame
     day = data[(data["TIMESTAMP"] >= datetime(current_date.year, current_date.month, current_date.day, 0, 0)) 
                    & (data["TIMESTAMP"] <= datetime(current_date.year, current_date.month, current_date.day, 23, 55))]
 
 # Create the Daily CSVs within desired directory
     
 ## File path to directory the csv files need to be saved to
-    filepath = Path('/Users/savannahsouthward/opt/anaconda3/envs/METR-2613/Data/csv/') 
-    
+    filepath = Path('/Users/savannahsouthward/opt/anaconda3/envs/METR-2613/Data/csv/')     
 ## File name that fills in the proper datetimes for the data
     filename = "NWC_{}{:02d}{:02d}.dat".format(current_date.year, current_date.month, current_date.day)
-    print(filename)
-    
+    print(filename)   
 ## CSV Outfile
     day.to_csv(filepath/filename)
     
-############################################################### 
+############################################################################################################################## 
 
-# Summary Reports
+# QA-ed Summary Reports
 
-## Set up daily dataframe from 00z to 23:55z using the raw data file
-
+## Set up daily dataframe from 00z to 23:55z for both a parsed day and calculations
     day = data.copy()
     day.reset_index(inplace = True)
     day['TIMESTAMP'] = pd.to_datetime(day['TIMESTAMP'])
@@ -154,12 +160,14 @@ while current_date <= end_date:
     daystats.reset_index(inplace = True)
     daystats['TIMESTAMP'] = pd.to_datetime(daystats['TIMESTAMP'])
     
-## Set up daily dataframe from 00z to 23:55z
+## Clarify the start and end time for each day
     start_time = datetime(current_date.year, current_date.month, current_date.day, 0, 0)
     end_time = datetime(current_date.year, current_date.month, current_date.day, 23, 55)
     
     daily = day[(day["TIMESTAMP"] >= start_time) & ((day["TIMESTAMP"] <= end_time))]
     dailystats = daystats[(daystats["TIMESTAMP"] >= start_time) & ((daystats["TIMESTAMP"] <= end_time))]
+
+# Calculations
 
 ## Number of lines missing from the data file
    ### Observations are taken at five minute intervals every hour for 24 hours.
